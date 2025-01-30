5213788ac2c416752d1777642afdb40ab763527c
karim-99-99 <kareem.hossam99@yahoo.com>
2025-01-30 12:42:38 +0200

    kkk

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ document.addEventListener("DOMContentLoaded", function () {
     let isValid = true;
     let messages = [];
 
-    const usernameInput = document.getElementById("username").value.trim();
-    const emailInput = document.getElementById("email").value.trim();
-    const passwordInput = document.getElementById("password").value.trim();
+    const username = document.getElementById("username").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
 
-    if (usernameInput.length < 3) {
+    if (username.length < 3) {
       isValid = false;
       messages.push("Username must be at least 3 characters long.");
     }
 
-    if (!emailInput.includes("@") || !emailInput.includes(".")) {
+    if (!email.includes("@") || !emailInput.includes(".")) {
       isValid = false;
       messages.push("please enter valid email");
     }
 
-    if (passwordInput.length < 8) {
+    if (password.length < 8) {
       isValid = false;
       messages.push("please enter valid password");
     }
